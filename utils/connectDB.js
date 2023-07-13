f09c4c8dca28e381af8542c7f66264ea3156ac15
Omae <larhnimikit@gmail.com>
2023-07-13 14:54:57 +0100

    full project front and back resolved credentiels

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 export default async function connectDB(){
     try {
-        await mongoose.connect('mongodb+srv://omar:larhnimi@cluster0.k3l58pi.mongodb.net/prj_1',
+        await mongoose.connect('mongodb+srv://<db_user>:<db_pass>@cluster0.k3l58pi.mongodb.net/prj_1',
         {useUnifiedTopology: true, useNewUrlParser: true})
         console.log('DB connection established')
     } catch (error) {
